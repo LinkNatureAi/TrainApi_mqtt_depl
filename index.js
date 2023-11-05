3428a0efb3f8a6ac88c929709454d8df90dc46d1
SONU KOL <108781856+LinkNatureAi@users.noreply.github.com>
2023-11-05 05:56:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const client = mqtt.connect('mqtt://broker.hivemq.com');
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
   setInterval(() => {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    client.publish('train', JSON.stringify({ value: randomNumber }));
+    try {
+      const randomNumber = Math.floor(Math.random() * 100) + 1;
+      client.publish('train', JSON.stringify({ value: randomNumber }));
+    } catch (error) {
+      console.error('Error publishing message:', error);
+    }
   }, 1000);
+});
+
+client.on('error', (error) => {
+  console.error('Error connecting to MQTT broker:', error);
 });
